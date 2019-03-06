8c3f9a5d50eee90e71d576e8264a604cddd9bfc7
deepakgouda <deepakgouda1729@gmail.com>
2019-03-06 12:48:38 +0000

    Fixed Server binding and added TCPClient.c

--- TCPServer.c
+++ TCPServer.c
@@ -5,13 +5,13 @@
 #include<netinet/in.h>
 
 #define backlog 5
-
+#define bufferSize 1024
 int main(int argc, char const *argv[])
 {
 	const char *serv_addr = argv[1];
 	const int serv_port = atoi(argv[2]);
 
-	char buffer[1024] = {0};
+	char buffer[bufferSize] = {0};
 	struct sockaddr_in server;
 	int sockfd, clientSocket, addrlen = sizeof(server);
 
@@ -23,16 +23,9 @@ int main(int argc, char const *argv[])
 	}
 
 	// Socket Binding
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(1);
-	}
-
 	server.sin_family = AF_INET;
 	server.sin_port = htons(serv_port);
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_family = AF_INET;
 
 	if(bind(sockfd, (struct sockaddr *)&server, 
 				sizeof(server)) < 0)
@@ -58,7 +51,7 @@ int main(int argc, char const *argv[])
 			exit(1);
 		}
 
-		char *val = read(clientSocket, buffer, 1024);
+		char *val = read(clientSocket, buffer, bufferSize);
 		printf("%s", buffer);
 		send(clientSocket, "Server Response", 
 						strlen("Server Response"), 0);
@@ -66,4 +59,3 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
-// 2553543 2
