52b985b47a71c0d0a5e291ec492d437b5af9677a
deepakgouda <deepakgouda1729@gmail.com>
2019-03-18 22:44:47 +0000

    Fixed GET partially; code indented

--- ftpclient.c
+++ ftpclient.c
@@ -1,5 +1,5 @@
 /*FTP Client*/
- 
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
@@ -16,6 +16,7 @@
 #include<fcntl.h>
 
 #define MAXSIZE 512
+#define BUFSIZE 600
 
 #define ACK                   2
 #define NACK                  3
@@ -30,199 +31,209 @@ int readn(int sd,char *ptr,int size);
 
 int main(int argc,char *argv[])
 {
-  struct sockaddr_in server;
-  struct stat obj;
-  int sock;
-  int choice;
-  char buf[100], command[5], filename[20], *f;
-  int k, size, status;
-  int filehandle;
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  if(sock == -1)
-    {
-      printf("socket creation failed");
-      exit(1);
-    }
-  server.sin_family = AF_INET;
-  server.sin_port = htons(atoi(argv[1]));
-  server.sin_addr.s_addr = 0;
-  k = connect(sock,(struct sockaddr*)&server, sizeof(server));
-  if(k == -1)
-    {
-      printf("Connect Error");
-      exit(1);
-    }
-  int i = 1;
-  while(1)
-    {
-      printf("Enter a choice:\n1- get\n2- put\n3- pwd\n4- ls\n5- cd\n6- quit\n");
-      scanf("%d", &choice);
-      switch(choice)
+	struct sockaddr_in server;
+	struct stat obj;
+	int sock;
+	int choice;
+	char buf[BUFSIZE], command[5], filename[20], *f;
+	int k, size, status;
+	int filehandle;
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock == -1)
 	{
-	case 1:
-	  printf("Enter filename to get: ");
-	  scanf("%s", filename);
-	  strcpy(buf, "get ");
-	  strcat(buf, filename);
-	  send(sock, buf, 100, 0);
-	  int msg_ok;
-	  recv(sock, &msg_ok, sizeof(int), 0);
-	  msg_ok = ntohs(msg_ok);
-	  printf("message: %d\n", msg_ok);
-	  int fail;
-	  recv(sock, &fail, sizeof(int), 0);
-	  fail = ntohs(fail);
-	  printf("message: %d\n", fail);
-	  recv(sock, &size, sizeof(int), 0);
-	  size = ntohs(size);
-	  printf("size: %d\n", size);
-
-	  if(!size)
-	    {
-	      printf("No such file on the remote directory\n\n");
-	    break;
-	    }
-	  int temp = 1;
-	  writen(sock,(char *)&temp,sizeof(temp));
-	  int num_blocks, num_last_blk;
-	  recv(sock, &num_blocks, sizeof(int), 0);
-	  num_blocks = ntohs(num_blocks);
-	  printf("No of blocks to recieve: %d\n", num_blocks);
-	  temp = htons(ACK);
-	  writen(sock,(char *)&temp,sizeof(temp));
-	  recv(sock, &num_last_blk, sizeof(int), 0);
-	  num_last_blk = ntohs(num_last_blk);
-	  printf("Last block size: %d\n", num_last_blk);
-	  writen(sock,(char *)&temp,sizeof(temp));
-
-	  /* ACTUAL FILE TRANSFER STARTS  BLOCK BY BLOCK*/ 
-
-	  printf("Creating empty file...\n");
-      FILE *fp = fopen(filename, "w"); 
-      fclose(fp);
-
-	  f = malloc(size);
-	  for(i= 0; i < num_blocks; i ++) { 
-	  	int no_read = recv(sock, f, MAXSIZE, 0);
-      if (no_read < 0) {printf("server: file recieve error\n");exit(0);}
-      if (no_read != MAXSIZE)
-              {printf("server: file recieve error : no_read is less\n");exit(0);}
-      printf(" %d...",i+1);
-      FILE *fp = fopen(filename, "a");
-      fputs(no_read, fp);
-      fclose(fp);
-      }
-
-   if (num_last_blk > 0) { 
-      int no_read = recv(sock, f, num_last_blk, 0);
-      if (no_read < 0) {printf("server: file recieve error\n");exit(0);}
-      if (no_read != num_last_blk)
-              {printf("server: file recieve error : no_read is less\n");exit(0);}
-      printf(" %d...\n",num_blocks + 1);
-      FILE *fp = fopen(filename, "a");
-      fputs(f, fp);
-      fclose(fp);
-      }
-	  strcpy(buf, "cat ");
-	  strcat(buf, filename);
-	  system(buf);
-	  printf("\n");
-	  break;
-	case 2:
-	  printf("Enter filename to put to server: ");
-          scanf("%s", filename);
-	  filehandle = open(filename, O_RDONLY);
-          if(filehandle == -1)
-            {
-              printf("No such file on the local directory\n\n");
-              break;
-            }
-          strcpy(buf, "put ");
-	  strcat(buf, filename);
-	  send(sock, buf, 100, 0);
-	  stat(filename, &obj);
-	  size = obj.st_size;
-	  send(sock, &size, sizeof(int), 0);
-	  sendfile(sock, filehandle, NULL, size);
-	  recv(sock, &status, sizeof(int), 0);
-	  if(status)
-	    printf("File stored successfully\n");
-	  else
-	    printf("File failed to be stored to remote machine\n");
-	  break;
-	case 3:
-	  strcpy(buf, "pwd");
-	  send(sock, buf, 100, 0);
-	  recv(sock, buf, 100, 0);
-	  printf("The path of the remote directory is: %s\n", buf);
-	  break;
-	case 4:
-	  strcpy(buf, "ls");
-          send(sock, buf, 100, 0);
-	  recv(sock, &size, sizeof(int), 0);
-          f = malloc(size);
-          recv(sock, f, size, 0);
-	  filehandle = creat("temp.txt", O_WRONLY);
-	  write(filehandle, f, size, 0);
-	  close(filehandle);
-          printf("The remote directory listing is as follows:\n");
-	  system("cat temp.txt");
-	  break;
-	case 5:
-	  strcpy(buf, "cd ");
-	  printf("Enter the path to change the remote directory: ");
-	  scanf("%s", buf + 3);
-          send(sock, buf, 100, 0);
-	  recv(sock, &status, sizeof(int), 0);
-          if(status)
-            printf("Remote directory successfully changed\n");
-          else
-            printf("Remote directory failed to change\n");
-          break;
-	case 6:
-	  strcpy(buf, "quit");
-          send(sock, buf, 100, 0);
-          recv(sock, &status, 100, 0);
-	  if(status)
-	    {
-	      printf("Server closed\nQuitting..\n");
-	      exit(0);
-	    }
-	    printf("Server failed to close connection\n");
+		printf("socket creation failed");
+		exit(1);
 	}
-    }
+	server.sin_family = AF_INET;
+	server.sin_port = htons(atoi(argv[1]));
+	server.sin_addr.s_addr = 0;
+	k = connect(sock,(struct sockaddr*)&server, sizeof(server));
+	if(k == -1)
+	{
+		printf("Connect Error");
+		exit(1);
+	}
+
+	int i = 1;
+	while(1)
+	{
+		printf("Enter a choice:\n1- get\n2- put\n3- pwd\n4- ls\n5- cd\n6- quit\n");
+		scanf("%d", &choice);
+		switch(choice)
+		{
+			case 1:
+				printf("Enter filename to get: ");
+				scanf("%s", filename);
+				strcpy(buf, "get ");
+				strcat(buf, filename);
+				send(sock, buf, 100, 0);
+				int msg_ok;
+				recv(sock, &msg_ok, sizeof(int), 0);
+				msg_ok = ntohs(msg_ok);
+				printf("message: %d\n", msg_ok);
+				int fail;
+				recv(sock, &fail, sizeof(int), 0);
+				fail = ntohs(fail);
+				printf("message: %d\n", fail);
+				recv(sock, &size, sizeof(int), 0);
+				size = ntohs(size);
+				printf("size: %d\n", size);
+
+				if(!size)
+				{
+					printf("No such file on the remote directory\n\n");
+					break;
+				}
+				int temp = 1;
+				writen(sock,(char *)&temp,sizeof(temp));
+				int num_blocks, num_last_blk;
+				recv(sock, &num_blocks, sizeof(int), 0);
+				num_blocks = ntohs(num_blocks);
+				printf("No of blocks to recieve: %d\n", num_blocks);
+				temp = htons(ACK);
+				writen(sock,(char *)&temp,sizeof(temp));
+				recv(sock, &num_last_blk, sizeof(int), 0);
+				num_last_blk = ntohs(num_last_blk);
+				printf("Last block size: %d\n", num_last_blk);
+				writen(sock,(char *)&temp,sizeof(temp));
+
+				/* ACTUAL FILE TRANSFER STARTS  BLOCK BY BLOCK*/ 
+
+				printf("Creating empty file...\n");
+				FILE *fp = fopen(filename, "w"); 
+				fclose(fp);
+
+
+				f = malloc(size);
+				for(i= 0; i < num_blocks; i ++) 
+				{ 
+					int no_read = recv(sock, f, MAXSIZE, 0);
+					if(no_read < 0)
+						{printf("server: file recieve error\n");exit(0);}
+					if(no_read != MAXSIZE)
+						{printf("server: file recieve error : no_read is less\n");exit(0);}
+					printf(" %d...\n", i+1);
+					FILE *fp = fopen(filename, "a");
+					fputs(f, fp);
+					fclose(fp);
+				}
+	
+				free(f);
+				f = malloc(size - num_blocks*MAXSIZE);
+				printf("%d\n", num_last_blk);
+	 			if(num_last_blk > 0)
+				{
+					int no_read = recv(sock, f, num_last_blk, 0);
+					if(no_read < 0) {printf("server: file recieve error\n");exit(0);}
+					if(no_read != num_last_blk)
+						{printf("server: file recieve error : no_read is less\n");exit(0);}
+					printf(" %d...\n",num_blocks + 1);
+					FILE *fp = fopen(filename, "a");
+					fputs(f, fp);
+					fclose(fp);
+				}
+				strcpy(buf, "cat ");
+				strcat(buf, filename);
+				system(buf);
+				printf("\n");
+				break;
+		case 2:
+				printf("Enter filename to put to server: ");
+				scanf("%s", filename);
+				filehandle = open(filename, O_RDONLY);
+				if(filehandle == -1)
+				{
+					printf("No such file on the local directory\n\n");
+					break;
+				}
+				strcpy(buf, "put ");
+				strcat(buf, filename);
+				send(sock, buf, 100, 0);
+				stat(filename, &obj);
+				size = obj.st_size;
+				send(sock, &size, sizeof(int), 0);
+				sendfile(sock, filehandle, NULL, size);
+				recv(sock, &status, sizeof(int), 0);
+				if(status)
+					printf("File stored successfully\n");
+				else
+					printf("File failed to be stored to remote machine\n");
+				break;
+		case 3:
+				strcpy(buf, "pwd");
+				send(sock, buf, 100, 0);
+				recv(sock, buf, 100, 0);
+				printf("The path of the remote directory is: %s\n", buf);
+				break;
+		case 4:
+				strcpy(buf, "ls");
+				send(sock, buf, 100, 0);
+				recv(sock, &size, sizeof(int), 0);
+				f = malloc(size);
+				recv(sock, f, size, 0);
+				filehandle = creat("temp.txt", O_WRONLY);
+				write(filehandle, f, size, 0);
+				close(filehandle);
+				printf("The remote directory listing is as follows:\n");
+				system("cat temp.txt");
+				break;
+		case 5:
+				strcpy(buf, "cd ");
+				printf("Enter the path to change the remote directory: ");
+				scanf("%s", buf + 3);
+				send(sock, buf, 100, 0);
+				recv(sock, &status, sizeof(int), 0);
+				if(status)
+					printf("Remote directory successfully changed\n");
+				else
+					printf("Remote directory failed to change\n");
+				break;
+		case 6:
+				strcpy(buf, "quit");
+				send(sock, buf, 100, 0);
+				recv(sock, &status, 100, 0);
+				if(status)
+				{
+					printf("Server closed\nQuitting..\n");
+					exit(0);
+				}
+				printf("Server failed to close connection\n");
+		}
+	}
 }
 
-
-/*
-  TO TAKE CARE OF THE POSSIBILITY OF BUFFER LIMMITS IN THE KERNEL FOR THE
- SOCKET BEING REACHED (WHICH MAY CAUSE READ OR WRITE TO RETURN FEWER CHARACTERS
-  THAN REQUESTED), WE USE THE FOLLOWING TWO FUNCTIONS */  
-   
+/* TO TAKE CARE OF THE POSSIBILITY OF BUFFER LIMMITS IN THE KERNEL FOR THE
+SOCKET BEING REACHED (WHICH MAY CAUSE READ OR WRITE TO RETURN FEWER CHARACTERS
+THAN REQUESTED), WE USE THE FOLLOWING TWO FUNCTIONS */  
+	 
 int readn(int sd,char *ptr,int size)
-{         int no_left,no_read;
-          no_left = size;
-          while (no_left > 0) 
-                     { no_read = read(sd,ptr,no_left);
-                       if(no_read <0)  return(no_read);
-                       if (no_read == 0) break;
-                       no_left -= no_read;
-                       ptr += no_read;
-                     }
-          return(size - no_left);
+{
+	int no_left,no_read;
+	no_left = size;
+	while (no_left > 0) 
+	{
+		no_read = read(sd,ptr,no_left);
+		if(no_read <0)
+			return(no_read);
+		if(no_read == 0)
+			break;
+	 	no_left -= no_read;
+	 	ptr += no_read;
+	}
+	return(size - no_left);
 }
 
 int writen(int sd,char *ptr,int size)
-{         int no_left,no_written;
-          no_left = size;
-          while (no_left > 0) 
-                     { no_written = write(sd,ptr,no_left);
-                       if(no_written <=0)  return(no_written);
-                       no_left -= no_written;
-                       ptr += no_written;
-                     }
-          return(size - no_left);
-}
-
-
-           
+{
+	int no_left,no_written;
+	no_left = size;
+	while (no_left > 0) 
+	{
+		no_written = write(sd,ptr,no_left);
+		if(no_written <=0)
+			return(no_written);
+		no_left -= no_written;
+		ptr += no_written;
+	}
+	return(size - no_left);
+}				 
--- ftpserver.c
+++ ftpserver.c
@@ -1,12 +1,12 @@
 
 /* This is the server for a very simple file transfer
-   service.  This is a "concurrent server" that can
-   handle requests from multiple simultaneous clients.
-   For each client:
-    - get file name and check if it exists
-    - send size of file to client
-    - send file to client, a block at a time
-    - close connection with client
+	 service.  This is a "concurrent server" that can
+	 handle requests from multiple simultaneous clients.
+	 For each client:
+		- get file name and check if it exists
+		- send size of file to client
+		- send file to client, a block at a time
+		- close connection with client
 */
 
 #include <stdio.h>
@@ -20,6 +20,7 @@
 #define MY_PORT_ID 8000
 #define MAXLINE 256
 #define MAXSIZE 512   
+#define BUFSIZE 600
 
 #define ACK                   2
 #define NACK                  3
@@ -34,230 +35,228 @@ int readn(int sd,char *ptr,int size);
 
 main()  {
 
-   int sockid, newsd, pid, clilen;
-   struct sockaddr_in my_addr, client_addr;   
+	 int sockid, newsd, pid, clilen;
+	 struct sockaddr_in my_addr, client_addr;   
 
-   printf("server: creating socket\n");
-   if ((sockid = socket(AF_INET,SOCK_STREAM,0)) < 0)
-     {printf("server: socket error : %d\n", errno); exit(0); }
+	 printf("server: creating socket\n");
+	 if ((sockid = socket(AF_INET,SOCK_STREAM,0)) < 0)
+		 {printf("server: socket error : %d\n", errno); exit(0); }
 
-   printf("server: binding my local socket\n");
-   bzero((char *) &my_addr,sizeof(my_addr));
-   my_addr.sin_family = AF_INET;
-   my_addr.sin_port = htons(MY_PORT_ID);
-   my_addr.sin_addr.s_addr = htons(INADDR_ANY);
-   if (bind(sockid ,(struct sockaddr *) &my_addr,sizeof(my_addr)) < 0)
-     {printf("server: bind  error :%d\n", errno); exit(0); }
-   printf("server: starting listen \n");
-   if (listen(sockid,5) < 0)
-     { printf("server: listen error :%d\n",errno);exit(0);}                                        
+	 printf("server: binding my local socket\n");
+	 bzero((char *) &my_addr,sizeof(my_addr));
+	 my_addr.sin_family = AF_INET;
+	 my_addr.sin_port = htons(MY_PORT_ID);
+	 my_addr.sin_addr.s_addr = htons(INADDR_ANY);
+	 if (bind(sockid ,(struct sockaddr *) &my_addr,sizeof(my_addr)) < 0)
+		 {printf("server: bind  error :%d\n", errno); exit(0); }
+	 printf("server: starting listen \n");
+	 if (listen(sockid,5) < 0)
+		 { printf("server: listen error :%d\n",errno);exit(0);}                                        
 
-   while(1) { 
-     /* ACCEPT A CONNECTION AND THEN CREATE A CHILD TO DO THE WORK */
-     /* LOOP BACK AND WAIT FOR ANOTHER CONNECTION                  */
-     printf("server: starting accept\n");
-     if ((newsd = accept(sockid ,(struct sockaddr *) &client_addr,
-                                      &clilen)) < 0)
-        {printf("server: accept  error :%d\n", errno); exit(0); }
-        printf("server: return from accept, socket for this ftp: %d\n",
-                                       newsd);
-     if ( (pid=fork()) == 0) {
-         /* CHILD PROC STARTS HERE. IT WILL DO ACTUAL FILE TRANSFER */
-         close(sockid);   /* child shouldn't do an accept */
-         doftp(newsd);
-         close (newsd);
-         exit(0);         /* child all done with work */
-         }
-      /* PARENT CONTINUES BELOW HERE */
-     close(newsd);        /* parent all done with client, only child */
-     }              /* will communicate with that client from now on */
+	 while(1) { 
+		 /* ACCEPT A CONNECTION AND THEN CREATE A CHILD TO DO THE WORK */
+		 /* LOOP BACK AND WAIT FOR ANOTHER CONNECTION                  */
+		 printf("server: starting accept\n");
+		 if ((newsd = accept(sockid ,(struct sockaddr *) &client_addr,
+																			&clilen)) < 0)
+				{printf("server: accept  error :%d\n", errno); exit(0); }
+				printf("server: return from accept, socket for this ftp: %d\n",
+																			 newsd);
+		 if ( (pid=fork()) == 0) {
+				 /* CHILD PROC STARTS HERE. IT WILL DO ACTUAL FILE TRANSFER */
+				 close(sockid);   /* child shouldn't do an accept */
+				 doftp(newsd);
+				 close (newsd);
+				 exit(0);         /* child all done with work */
+				 }
+			/* PARENT CONTINUES BELOW HERE */
+		 close(newsd);        /* parent all done with client, only child */
+		 }              /* will communicate with that client from now on */
 }   
-     
+		 
 
 /* CHILD PROCEDURE, WHICH ACTUALLY DOES THE FILE TRANSFER */
-doftp(int newsd)
-  {       
-    int i,fsize,fd,msg_ok,fail,fail1,req,c,ack;
-    int no_read ,num_blks , num_blks1,num_last_blk,num_last_blk1,tmp;
-    char fname[MAXLINE];
-    char out_buf[MAXSIZE];
-    char buf[100];
-    FILE *fp;
-      
-     no_read = 0;
-     num_blks = 0;
-     num_last_blk = 0; 
-
-    
-     /* START SERVICING THE CLIENT */ 
-  
-     /* get command code from client.*/
-     /* only one supported command: 100 -  get a file */
-     req = 0;
-     char rcv[100];
-     req = recv(newsd, rcv, 100, 0);
-     if(req < 0)
-	 {printf("server: read error %d\n",errno);exit(0);}
-     printf("server: client request code is: %d\n",req);
-     if (req!=REQUESTFILE) {
-	 printf("server: unsupported operation. goodbye\n");
-         /* reply to client: command not OK  (code: 150) */
-         msg_ok = COMMANDNOTSUPPORTED; 
-         msg_ok = htons(msg_ok);
-         if((writen(newsd,(char *)&msg_ok,sizeof(msg_ok))) < 0)
-            {printf("server: write error :%d\n",errno);exit(0);}
-         exit(0);
-         }
-
-    i = 0;
-    while((i < strlen(rcv)) && (rcv[i] != ' '))
-    	i++;
-    int j = 0;
-    i += 1;
-    while(i < strlen(rcv)){
-    	fname[j] = rcv[i];
-    	j++;
-    	i++;
-    }
-    fname[j] = '\0';
-
-     /* reply to client: command OK  (code: 160) */
-     msg_ok = COMMANDSUPPORTED; 
-     msg_ok = htons(msg_ok);
-     if((writen(newsd,(char *)&msg_ok,sizeof(msg_ok))) < 0)
-             {printf("server: write error :%d\n",errno);exit(0);}
-  
-    fail = FILENAMEOK;
-   
-     /* IF SERVER CANT OPEN FILE THEN INFORM CLIENT OF THIS AND TERMINATE */
-     if((fp = fopen(fname,"r")) == NULL) /*cant open file*/
-        fail = BADFILENAME;
-
-     tmp = htons(fail);
-     if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0)
-        {printf("server: write error :%d\n",errno);exit(0);   }
-     if(fail == BADFILENAME) {printf("server cant open file\n");
-                            close(newsd);exit(0);}
-     printf("server: filename is %s\n",fname);
-
-     fseek(fp, 0 , SEEK_END);
-     fsize = ftell(fp);
-     fseek(fp, 0 , SEEK_SET);// needed for next read from beginning of file
-     tmp = htons(fsize);
-     if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0)
-        {printf("server: write error :%d\n",errno);exit(0);   }
-
-  
-    req = -1;
-    recv(newsd,&req,sizeof(req), 0);
-    printf("%d\n", req);
-    if (req < 0)
-       		      {printf("server: read error :%d\n",errno);exit(0);}
-    printf("server: start transfer command, %d, received\n", req);
-
-   
-   /*SERVER GETS FILESIZE AND CALCULATES THE NUMBER OF BLOCKS OF 
-     SIZE = MAXSIZE IT WILL TAKE TO TRANSFER THE FILE. ALSO CALCULATE
-     NUMBER OF BYTES IN THE LAST PARTIALLY FILLED BLOCK IF ANY. 
-     SEND THIS INFO TO CLIENT, RECEIVING ACKS */
-    printf("server: starting transfer\n");
-    ack = 0;
-    num_blks = fsize / MAXSIZE; 
-    num_blks1 = htons(num_blks);
-    num_last_blk = fsize % MAXSIZE; 
-    num_last_blk1 = htons(num_last_blk);
-    if((writen(newsd,(char *)&num_blks1,sizeof(num_blks1))) < 0)
-             {printf("server: write error :%d\n",errno);exit(0);}
-    printf("server: told client there are %d blocks\n", num_blks);  
-    if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
-        {printf("server: ack read error :%d\n",errno);exit(0); }          
-    if (ntohs(ack) != ACK) {
-      printf("client: ACK not received on file size\n");
-      exit(0);
-      }
-    if((writen(newsd,(char *)&num_last_blk1,sizeof(num_last_blk1))) < 0)
-       {printf("server: write error :%d\n",errno);exit(0);}
-    printf("server: told client %d bytes in last block\n", num_last_blk);  
-    if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
-        {printf("server: ack read error :%d\n",errno);exit(0); }
-    if (ntohs(ack) != ACK) {
-      printf("server: ACK not received on file size\n");
-      exit(0);
-      }
-
-    /* ACTUAL FILE TRANSFER STARTS  BLOCK BY BLOCK*/ 
+void doftp(int newsd)
+{
+	int i,fsize,fd,msg_ok,fail,fail1,req,c,ack;
+	int no_read ,num_blks , num_blks1,num_last_blk,num_last_blk1,tmp;
+	char fname[MAXLINE];
+	char out_buf[MAXSIZE];
+	char buf[BUFSIZE];
+	FILE *fp;
+		
+	no_read = 0;
+	num_blks = 0;
+	num_last_blk = 0; 
 
 
-    rewind(fp);          
-  for(i= 0; i < num_blks; i ++) { 
-      no_read = fread(out_buf,sizeof(char),MAXSIZE,fp);
-      if (no_read == 0) {printf("server: file read error\n");exit(0);}
-      if (no_read != MAXSIZE)
-              {printf("server: file read error : no_read is less\n");exit(0);}
-      if((writen(newsd,out_buf,MAXSIZE)) < 0)
-                 {printf("server: error sending block:%d\n",errno);exit(0);}
-      if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
-                 {printf("server: ack read  error :%d\n",errno);exit(0);}
-      if (ntohs(ack) != ACK) {
-          printf("server: ACK not received for block %d\n",i);
-          exit(0);
-          }
-      printf(" %d...",i);
-      }
+	/* START SERVICING THE CLIENT */ 
 
-   if (num_last_blk > 0) { 
-      printf("%d\n",num_blks);
-      no_read = fread(out_buf,sizeof(char),num_last_blk,fp); 
-      if (no_read == 0) {printf("server: file read error\n");exit(0);}
-      if (no_read != num_last_blk) 
-            {printf("server: file read error : no_read is less 2\n");exit(0);}
-      if((writen(newsd,out_buf,num_last_blk)) < 0)
-                 {printf("server: file transfer error %d\n",errno);exit(0);}
-      if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
-	         {printf("server: ack read  error %d\n",errno);exit(0);}
-      if (ntohs(ack) != ACK) {
-          printf("server: ACK not received last block\n");
-          exit(0);
-          }
-      }
-    else printf("\n");
-                                                  
-   /* FILE TRANSFER ENDS */
-   printf("server: FILE TRANSFER COMPLETE on socket %d\n",newsd);
-   fclose(fp);
-   close(newsd);
-  }
+	/* get command code from client.*/
+	/* only one supported command: 100 -  get a file */
+	req = 0;
+	char rcv[BUFSIZE];
+	req = recv(newsd, rcv, BUFSIZE, 0);
+	if(req < 0)
+		{printf("server: read error %d\n",errno);exit(0);}
+	printf("server: client request code is: %d\n",req);
+	if(req!=REQUESTFILE)
+	{
+		printf("server: unsupported operation. goodbye\n");
+		/* reply to client: command not OK  (code: 150) */
+		msg_ok = COMMANDNOTSUPPORTED; 
+		msg_ok = htons(msg_ok);
+		if((writen(newsd,(char *)&msg_ok,sizeof(msg_ok))) < 0)
+			{printf("server: write error :%d\n",errno);exit(0);}
+		exit(0);
+	}
 
+	i = 0;
+	while((i < strlen(rcv)) && (rcv[i] != ' '))
+		i++;
+	int j = 0;
+	i += 1;
+	while(i < strlen(rcv))
+	{
+		fname[j] = rcv[i];
+		j++;
+		i++;
+	}
+	fname[j] = '\0';
 
-/*
-  TO TAKE CARE OF THE POSSIBILITY OF BUFFER LIMMITS IN THE KERNEL FOR THE
- SOCKET BEING REACHED (WHICH MAY CAUSE READ OR WRITE TO RETURN FEWER CHARACTERS
-  THAN REQUESTED), WE USE THE FOLLOWING TWO FUNCTIONS */  
-   
+	/* reply to client: command OK  (code: 160) */
+	msg_ok = COMMANDSUPPORTED; 
+	msg_ok = htons(msg_ok);
+	if((writen(newsd,(char *)&msg_ok,sizeof(msg_ok))) < 0)
+	 {printf("server: write error :%d\n",errno);exit(0);}
+
+	fail = FILENAMEOK;
+	 
+	/* IF SERVER CANT OPEN FILE THEN INFORM CLIENT OF THIS AND TERMINATE */
+	if((fp = fopen(fname,"r")) == NULL) /*cant open file*/
+		fail = BADFILENAME;
+
+	tmp = htons(fail);
+	if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0)
+		{printf("server: write error :%d\n",errno);exit(0);}
+	if(fail == BADFILENAME)
+	{
+		printf("server cant open file\n");
+		close(newsd);exit(0);
+	}
+	printf("server: filename is %s\n",fname);
+
+	fseek(fp, 0 , SEEK_END);
+	fsize = ftell(fp);
+	fseek(fp, 0 , SEEK_SET);// needed for next read from beginning of file
+	tmp = htons(fsize);
+	if((writen(newsd,(char *)&tmp,sizeof(tmp))) < 0)
+		{printf("server: write error :%d\n",errno);exit(0);}
+
+	req = -1;
+	recv(newsd,&req,sizeof(req), 0);
+	printf("%d\n", req);
+	if (req < 0)
+		{printf("server: read error :%d\n",errno);exit(0);}
+	printf("server: start transfer command, %d, received\n", req);
+
+	 
+	/*SERVER GETS FILESIZE AND CALCULATES THE NUMBER OF BLOCKS OF 
+	SIZE = MAXSIZE IT WILL TAKE TO TRANSFER THE FILE. ALSO CALCULATE
+	NUMBER OF BYTES IN THE LAST PARTIALLY FILLED BLOCK IF ANY. 
+	SEND THIS INFO TO CLIENT, RECEIVING ACKS */
+	printf("server: starting transfer\n");
+	ack = 0;
+	num_blks = fsize / MAXSIZE; 
+	num_blks1 = htons(num_blks);
+	num_last_blk = fsize % MAXSIZE; 
+	num_last_blk1 = htons(num_last_blk);
+	if((writen(newsd,(char *)&num_blks1,sizeof(num_blks1))) < 0)
+		{printf("server: write error :%d\n",errno);exit(0);}
+	printf("server: told client there are %d blocks\n", num_blks);  
+	if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
+		{printf("server: ack read error :%d\n",errno);exit(0);}
+	if(ntohs(ack) != ACK)
+	{
+		printf("client: ACK not received on file size\n");
+		exit(0);
+	}
+	if((writen(newsd,(char *)&num_last_blk1,sizeof(num_last_blk1))) < 0)
+	 {printf("server: write error :%d\n",errno);exit(0);}
+	printf("server: told client %d bytes in last block\n", num_last_blk);
+	if((readn(newsd,(char *)&ack,sizeof(ack))) < 0)
+		{printf("server: ack read error :%d\n",errno);exit(0);}
+	if(ntohs(ack) != ACK)
+	{
+		printf("server: ACK not received on file size\n");
+		exit(0);
+	}
+
+	/* ACTUAL FILE TRANSFER STARTS  BLOCK BY BLOCK*/ 
+	rewind(fp);          
+	for(i= 0; i < num_blks; i ++) 
+	{ 
+		no_read = fread(out_buf,sizeof(char),MAXSIZE,fp);
+		if (no_read == 0) {printf("server: file read error\n");exit(0);}
+		if (no_read != MAXSIZE)
+			{printf("server: file read error : no_read is less\n");exit(0);}
+		if((writen(newsd,out_buf,MAXSIZE)) < 0)
+			{printf("server: error sending block:%d\n",errno);exit(0);}
+		printf(" %d...\n",i);
+	}
+
+	fseek(fp, num_blks*MAXSIZE, SEEK_SET);
+	printf("%d\n", num_last_blk);
+
+	if(num_last_blk > 0)
+	{ 
+		no_read = fread(out_buf,sizeof(char),num_last_blk,fp);
+		if (no_read == 0) {printf("server: file read error\n");exit(0);}
+		if (no_read != num_last_blk) 
+					{printf("server: file read error : no_read is less 2\n");exit(0);}
+		if((writen(newsd,out_buf,num_last_blk)) < 0)
+				{printf("server: file transfer error %d\n",errno);exit(0);}
+	}
+	else
+		printf("\n");
+
+	/* FILE TRANSFER ENDS */
+	printf("server: FILE TRANSFER COMPLETE on socket %d\n",newsd);
+	fclose(fp);
+	close(newsd);
+}
+
+/*TO TAKE CARE OF THE POSSIBILITY OF BUFFER LIMMITS IN THE KERNEL FOR THE
+SOCKET BEING REACHED (WHICH MAY CAUSE READ OR WRITE TO RETURN FEWER CHARACTERS
+THAN REQUESTED), WE USE THE FOLLOWING TWO FUNCTIONS */  
+	 
 int readn(int sd,char *ptr,int size)
-{         int no_left,no_read;
-          no_left = size;
-          while (no_left > 0) 
-                     { no_read = read(sd,ptr,no_left);
-                       if(no_read <0)  return(no_read);
-                       if (no_read == 0) break;
-                       no_left -= no_read;
-                       ptr += no_read;
-                     }
-          return(size - no_left);
+{
+	int no_left,no_read;
+	no_left = size;
+	while (no_left > 0) 
+	{
+		no_read = read(sd,ptr,no_left);
+		if(no_read <0)
+			return(no_read);
+		if(no_read == 0)
+			break;
+		no_left -= no_read;
+		ptr += no_read;
+	}
+	return(size - no_left);
 }
 
 int writen(int sd,char *ptr,int size)
-{         int no_left,no_written;
-          no_left = size;
-          while (no_left > 0) 
-                     { no_written = write(sd,ptr,no_left);
-                       if(no_written <=0)  return(no_written);
-                       no_left -= no_written;
-                       ptr += no_written;
-                     }
-          return(size - no_left);
+{
+	int no_left,no_written;
+	no_left = size;
+	while(no_left > 0) 
+	{
+		no_written = write(sd,ptr,no_left);
+		if(no_written <=0)  return(no_written);
+		no_left -= no_written;
+		ptr += no_written;
+	}
+	return(size - no_left);
 }
-
-
-           
